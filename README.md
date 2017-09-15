6470d2afe912c455b84729a0b2d2625e0e62a840
Alek Petty <alekpetty@gmail.com>
2017-09-15 16:33:29 -0400

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Arctic sea ice forecasting
-**Alek Petty**
+**Alek Petty**   
 **Add other contributors here**
 
 *Python scripts used to analyze waveforms from the ATM and upcoming ICESat-2 laser*
@@ -7,16 +7,33 @@
 
 ### Scripts
 
-* atmFuncs.py
+* atmFuncs.py*
  - Experimental ATM waveform functions provided by the ATM team (courtesy M. Studinger).
 
- * atmWaveform.ipynb
+ * atmWaveform.ipynb*
+  - iPython Notebook for exploring waveform characterization
 
 All processing in Python 2.7?
 
-Maybe add a list of Python dependencies here?
+Maybe add a list of Python dependencies here? Or even a 
 
 ### Data
 
-Add in some info on the ATM waveform data
+ILATM1B_20170309_133117.atm6AT6.snippet.h5
+ILATM1B_20170309_133117.atm6AT6.waveformDebug.h5
+ATM_waveform_product_user_manual_v06.pdf
 
+### DataOutput
+
+Folder for derived data products.
+
+### DataOutput
+
+Folder for figure outputs.
+
+
+### Ideas
+
+Look into different waveform classification methods.   
+Use Pandas to store and characterize the data.			
+Look into Dask library for scaling up the processing.
--- Scripts/atmFuncs.py
+++ Scripts/atmFuncs.py
@@ -24,21 +24,43 @@ def printContents(name, item):
 		print '{}{} ({})'.format(tab, name.split('/')[-1], len(item))
 	
 
-def getWaveform(ii):
-	
+def getWaveform(f):
+
 	# Account for the fact that python begins indexing at 0, but HDF5 file starts indexing at 1
 	index_base = 1
+	
+	# Load waveform-specific parameters from HDF5 file
+	shot_gate_starts = f['waveforms/twv/shot/gate_start'][:] - index_base # pointers to gate_len and gate_pos for each laser shot
+	num_gatess = f['waveforms/twv/shot/gate_count'][:] # number of gates for each laser shot
+	gate_lens = f['waveforms/twv/gate/wvfm_length'][:] # number of samples in the gate for each gate
+	gate_poss = f['waveforms/twv/gate/position'][:] # sample position for the start of each gate
+	wf_pointers = f['waveforms/twv/gate/wvfm_start'][:] # pointer to wvfm_amplitude to retrieve amplitudes for each gate
+	wf_amps = f['waveforms/twv/wvfm/amplitude'][:] # waveform amplitude values
 
-	shot_gate_start = shot_gate_starts[ii] # pointer to gate_len and gate_pos
-	num_gates = num_gatess[ii] # number of gates for laser shot
-	gate_len = gate_lens[shot_gate_start:shot_gate_start+num_gates] # list of gate lengths for each gate in waveform
-	gate_pos = gate_poss[shot_gate_start:shot_gate_start+num_gates] # list of sample position for each gate in waveform
-	wf_pointer = wf_pointers[shot_gate_start] - index_base # pointer to wvfm_amplitude to retrieve amplitudes for first gate
-	waveformx, waveformy = [], [] # initialize empty lists to hold waveform x- and y-values for each gate
-	for gate in range(num_gates): # loop through each gate in waveform
-		wfx = np.arange(float(gate_pos[gate]), gate_pos[gate]+gate_len[gate]) # create an array for gate x-values
-		wfy = wf_amps[wf_pointer:wf_pointer+gate_len[gate]] # extract waveform amplitudes from wf_pointer to wf_pointer+gate_len
-		waveformy.append(wfy) # add current gate y-values to list
-		waveformx.append(wfx) # add current gate x-values to list
-		wf_pointer = wf_pointer+gate_len[gate] # increment wf_pointer to start of next gate
-	return waveformx, waveformy
+	# Examples of loading other useful parameters (unused in this script)
+	utc = f['time/seconds_of_day'][:]  # UTC seconds of day
+
+	# Loop through all laser shot records and extract waveforms
+	nrec = len(utc) # calculate number of laser shot records
+	waveforms_x, waveforms_y = [], [] # initialize empty lists to hold x- and y- values for waveforms
+	
+
+	for i in range(nrec):  # loop through laser shots...
+		#wfx, wfy = aF.getWaveform(f, i) # call waveform extraction function
+
+		shot_gate_start = shot_gate_starts[i] # pointer to gate_len and gate_pos
+		num_gates = num_gatess[i] # number of gates for laser shot
+		gate_len = gate_lens[shot_gate_start:shot_gate_start+num_gates] # list of gate lengths for each gate in waveform
+		gate_pos = gate_poss[shot_gate_start:shot_gate_start+num_gates] # list of sample position for each gate in waveform
+		wf_pointer = wf_pointers[shot_gate_start] - index_base # pointer to wvfm_amplitude to retrieve amplitudes for first gate
+		waveformx, waveformy = [], [] # initialize empty lists to hold waveform x- and y-values for each gate
+		for gate in range(num_gates): # loop through each gate in waveform
+			wfx = np.arange(float(gate_pos[gate]), gate_pos[gate]+gate_len[gate]) # create an array for gate x-values
+			wfy = wf_amps[wf_pointer:wf_pointer+gate_len[gate]] # extract waveform amplitudes from wf_pointer to wf_pointer+gate_len
+			waveformy.append(wfy) # add current gate y-values to list
+			waveformx.append(wfx) # add current gate x-values to list
+			wf_pointer = wf_pointer+gate_len[gate] # increment wf_pointer to start of next gate
+		waveforms_x.append(waveformx)
+		waveforms_y.append(waveformy)
+
+	return waveforms_x, waveforms_y, num_gatess
